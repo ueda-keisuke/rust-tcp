aab259ad17cd0047e526f2194593877885003a5b
Keisuke Ueda <ueda.keisuke@me.com>
2022-08-20 13:49:06 -0700

    Add main.rs. Some modules have not been implemented.

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::env;
+#[macro_use]
+extern crate log;
+
+mod tcp_client;
+mod tcp_server;
+mod udp_client;
+mod udp_server;
+
+fn main() {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4
+    {
+        error!("Usage: [tcp|udp] [server|client] [addr:port]");
+        std::process::exit(1);
+    }
+
+    let protocol: &str = &args[1];
+    let role: &str = &args[2];
+    let address: &str = &args[3];
+
+    match protocol{
+        "tcp" => match role{
+            "server" => {
+                // TODO: call TCP server
+            }
+            "client" => {
+                // TODO: call TCP client
+            }
+            _ => {
+                missing_role();
+            }
+        },
+        "udp" => match role {
+            "server" => {
+                // TODO: call UDP server
+            }
+            "client" =>{
+                // TODO: call UDP client
+            }
+            _ => {
+                missing_role();
+            }
+        },
+        _ => {
+            error!("Please specify tcp or udp at the first argument.");
+            std::process::exit(1);
+        }
+    }
+}
+
+
+fn missing_role()
+{
+    error!("Please specify server or client at the second argument.");
+    std::process::exit(1);
+}
